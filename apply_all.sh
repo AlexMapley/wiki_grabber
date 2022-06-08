81f4ec0c2145cb6f4954e34c764cbfb06c2db7a8
AlexMapley <alexmapley@gmail.com>
2022-06-07 22:42:36 -0400

    update apply_all.sh comments

--- apply_all.sh
+++ apply_all.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
 # Helper script to apply all our k8s manifests.
-# We'll create our persistent volume via our pvc,
-# and reference that pvc in our cronjob spec.
+# We'll first create our persistent volume,
+# then our persistent volume claim,
+# and then reference that pvc in our cronjob spec.
 kubectl apply -f pv.yaml
 kubectl apply -f pvc.yaml
 kubectl apply -f cronjob.yaml
+
+# We create a stateful-set which mounts our persistent volume as well,
+# just for demoing purposes.
 kubectl apply -f sts.yaml
 
 # Display objects afterwards
