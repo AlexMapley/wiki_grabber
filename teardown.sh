7701027d9e063c367da2e6b02197c8179bce0faf
AlexMapley <alexmapley@gmail.com>
2022-06-07 22:12:00 -0400

    update teardown and cronjob.yaml

--- teardown.sh
+++ teardown.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-
+#
 # Helper script to easily delete all our k8s objects
+kubectl delete cronjob.batch/wiki-grabber
+kubectl delete statefulset.apps/volume-inspector 
 kubectl delete pvc/wiki-grabber-claim
 kubectl delete pv/wiki-grabber
-kubectl delete cronjob.batch/wiki-grabber
