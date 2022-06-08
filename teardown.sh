cea743a7a9c3c96b5764027141052255f5db8020
Al <alexmapley@gmail.com>
2022-06-08 11:17:23 -0400

    Update teardown.sh

--- teardown.sh
+++ teardown.sh
@@ -3,6 +3,6 @@
 # Helper script to easily delete all our k8s objects
 kubectl delete cronjob.batch/wiki-grabber
 kubectl delete statefulset.apps/volume-inspector
-kubectl delete pv/wiki-grabber
 kubectl delete pvc/wiki-grabber-claim
+kubectl delete pv/wiki-grabber
 
